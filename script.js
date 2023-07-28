cbbcd40f87219d43b9908dae82e91c6bbad1985f
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-07-28 07:33:26 +0200

    Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -38,21 +38,21 @@ document.addEventListener('keydown', function (e) {
 //////////////////////////////////////////
 
 // Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
 const header = document.querySelector(`.header`);
 const allSections = document.querySelectorAll(`.section`);
-console.log(allSections);
+// console.log(allSections);
 
-console.log(document.getElementById(`section--1`));
-console.log(document.querySelector(`#section--1`));
+// console.log(document.getElementById(`section--1`));
+// console.log(document.querySelector(`#section--1`));
 
 const allButtons = document.getElementsByTagName(`button`);
-console.log(allButtons);
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName(`btn`));
+// console.log(document.getElementsByClassName(`btn`));
 
 // Creating and inserting elements
 //.insertAdjacentHTML
@@ -81,3 +81,46 @@ document
   .addEventListener(`click`, function () {
     message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = `#37383d`;
+message.style.width = `120%`;
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + `px`;
+
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+// Attributes
+const logo = document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = `Beautiful minimalist logo`;
+
+// Non-standerd
+console.log(logo.designer);
+console.log(logo.getAttribute(`designer`));
+console.log(logo.setAttribute(`company`, `Bankist`));
+
+console.log(logo.getAttribute(`src`));
+
+const link = document.querySelector(`.nav__link--btn`);
+console.log(link.href);
+console.log(link.getAttribute(`href`));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add(`c`);
+logo.classList.remove(`c`);
+logo.classList.toggle(`c`);
+logo.classList.constians(`c`);
