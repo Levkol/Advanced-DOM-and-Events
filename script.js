da4b13d57dd0288e19d81be60159100f5fb4b1f2
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-07-30 18:11:50 +0200

    DOM Traversing

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector(`.btn--scroll-to`);
 const section1 = document.querySelector(`#section--1`);
 
-
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -79,18 +78,16 @@ btnScrollTo.addEventListener(`click`, function (e) {
 // 1. Add event listener to common parent element
 
 // 2. Determine what element originated the even
-document.querySelector(`.nav__links`).addEventListener(`click`, function(e){
-      e.preventDefault()
-;
-  
-  // Matching strategy
-  if(e.target.classList.contains(`nav__link`)) {
-        const id = e.target.getAttribute(`href`);
-    console.log(id);
-    document.querySelector(id).scrollIntoView({behavior: `smooth`})
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  e.preventDefault();
 
+  // Matching strategy
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
   }
-})
+});
 //////////////////////////////////////////
 //////////////////////////////////////////
 //////////////////////////////////////////
@@ -224,3 +221,31 @@ document.querySelector(`.nav`).addEventListener(`click`, function (e) {
   console.log(`NAV`, e.target, e.currentTarget);
 });
 */
+
+const h1 = document.querySelector(`h1`);
+
+// Going downwards: child
+console.log(h1.querySelectorAll(`.highlight`));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = `white`;
+h1.lastElementChild.style.color = `black`;
+
+// Going upwords: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(`.header`).style.background = `var(--gradient-secondary)`;
+h1.closest(`h1`).style.background = `var(--gradient-primary)`;
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = `scale(0.5)`;
+});
